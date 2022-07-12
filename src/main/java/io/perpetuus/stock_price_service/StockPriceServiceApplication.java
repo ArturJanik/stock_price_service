e7c8a70db7e997f6f175079ef4ed36924a1739b6
arturjanik <sidsky@o2.pl>
2022-07-12 20:54:32 +0200

    COMMIT-1 | Initial spring setup with mongo

---
+++ src/main/java/io/perpetuus/stock_price_service/StockPriceServiceApplication.java
@@ -0,0 +1,13 @@
+package io.perpetuus.stock_price_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPriceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPriceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/perpetuus/stock_price_service/StockPriceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.perpetuus.stock_price_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPriceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
