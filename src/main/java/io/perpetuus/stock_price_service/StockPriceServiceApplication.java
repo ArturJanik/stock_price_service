88b05d77c79151965ed4f912009a41e0d93e0b47
arturjanik <sidsky@o2.pl>
2022-07-12 23:50:51 +0200

    COMMIT-3 | Change config to build and deploy

--- src/main/java/io/perpetuus/stock_price_service/StockPriceServiceApplication.java
+++ src/main/java/io/perpetuus/stock_price_service/StockPriceServiceApplication.java
@@ -2,9 +2,10 @@ package io.perpetuus.stock_price_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StockPriceServiceApplication {
+public class StockPriceServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockPriceServiceApplication.class, args);
